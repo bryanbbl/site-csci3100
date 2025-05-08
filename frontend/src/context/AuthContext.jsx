c06f7fea5494866ae1cbe3e28c8d588d14391816
Tang Yuk Yee <tangyukyee@TangdeMacBook-Pro.local>
2025-05-08 18:44:09 +0800

    Fix AuthContext login request error

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:5000'
+axios.defaults.baseURL = 'http://localhost:5000';
 
 const AuthContext = createContext();
 
